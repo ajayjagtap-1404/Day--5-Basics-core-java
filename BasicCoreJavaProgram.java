cc71e2e1395a3c948f4814c1e295757bde8fa352
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-02-28 20:40:16 +0530

    Added : Code for showing welcome msg

---
+++ BasicCoreJavaProgram.java
@@ -0,0 +1,7 @@
+package com.assigenement.day_5.basic_core_java;
+
+public class BasicCoreJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Basic java Program :");
+    }
+}
