15cacbaa752ec33fc2b8d89885e92c9be9b56e86
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-02-28 21:26:11 +0530

    Added : code for to find powers of two

--- BasicCoreJavaProgram.java
+++ BasicCoreJavaProgram.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 public class BasicCoreJavaProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to Basic java Program :");
+     //   Scanner sc = new Scanner(System.in);
 
+        BasicCoreJavaProgram basicCoreJavaProgram = new BasicCoreJavaProgram();
+        basicCoreJavaProgram.findPowerOfTwo();
+
+    }
+     void findPowerOfTwo() {
+        int Total = 0;
+        System.out.println("Enter the Power of 2:");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter year you want to check Leap year or not :");
-        int year = sc .nextInt();
-        if (year > 1000 && year < 9999) {
-
-            if( (year % 4 == 0 ) && (year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println(year + " Leap Year ");
-            } else {
-                System.out.println(year + " not  Leap Year ");
-
-            }
-        } else {
-            System.out.println("please Enter four digit number");
+        int Power = sc.nextInt();
+        for (int i = 0; i <= Power; i++) {
+            Total = (int) Math.pow(2, i);
+            System.out.print(" power is : " + Total);
         }
-
     }
 }
